77dc8691c70a2f16e2fbe9b193715f5efbc15e5b
Balakrishna063 <balakrishnachintu357@gmail.com>
2025-02-26 10:23:17 +0530

    message

--- main-file/component/breadcrumb/BreadcrumbSection.tsx
+++ main-file/component/breadcrumb/BreadcrumbSection.tsx
@@ -1,30 +1,44 @@
-'use client'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { url } from "inspector";
+import Link from "next/link";
+import React from "react";
 
 type Props = {
-    header: string;
-    title: string;
-}
+  header: string;
+  title: string;
+  backgroundImage : string;
+};
 
-const BreadcrumbSection = ({header,title} : Props) => {
+const BreadcrumbSection = ({ header, title, backgroundImage }: Props) => {
   return (
-    <section className="tf__breadcrumb">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="tf__breadcrumb_text">
-                        <h2>{header}</h2>
-                        <ul>
-                            <li><Link href="/">Home</Link></li>
-                            <li><a href="#">{title}</a></li>
-                        </ul>
-                    </div>
-                </div>
+    <section
+      className="tf__breadcrumb"
+      style={{
+        backgroundImage: backgroundImage
+          ? `url(${backgroundImage})`
+          : undefined,
+      }}
+      aria-label="Breadcrumb Navigation"
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="tf__breadcrumb_text">
+              <h2>{header}</h2>
+              <ul>
+                <li>
+                  <Link href="/">Home</Link>
+                </li>
+                <li>
+                  <a href="#">{title}</a>
+                </li>
+              </ul>
             </div>
+          </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default BreadcrumbSection
+export default BreadcrumbSection;
